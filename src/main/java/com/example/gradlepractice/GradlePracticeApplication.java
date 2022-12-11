09824dd9fa5b3392eb6ccddc82e5c207c98b9d0a
worker <>
2022-12-11 16:48:44 +0800

    init

---
+++ src/main/java/com/example/gradlepractice/GradlePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradlePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlepractice/temp/HelloJunit.java
@@ -0,0 +1,13 @@
+package com.example.gradlepractice.temp;
+
+import org.junit.Test;
+
+public class HelloJunit {
+
+    @Test
+    public void t(){
+        System.out.println("dddd");
+    }
+
+
+}
